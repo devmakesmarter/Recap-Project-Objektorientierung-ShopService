791c5e368de9547da66a995cfe0adfec09965a62
Ismail Erdogan <erdogan.isma@gmail.com>
2025-10-28 15:32:27 +0100

    Neues Update 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
-    public static void main (String args []){
+    public static void main (String[] args){
 
         ShopService shopService = new ShopService();
         System.out.println(shopService);
-
+        System.out.println("Hallo");
 
 
     }

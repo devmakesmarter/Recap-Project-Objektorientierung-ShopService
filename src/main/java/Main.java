6997899318fd6ae14f23a6edd0af083b49b6c7ee
Ismail Erdogan <erdogan.isma@gmail.com>
2025-10-29 10:04:18 +0100

    Filtern nach OrderStatus implementiert.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main (String[] args){
 
         ShopService shopService = new ShopService();
-        System.out.println(shopService);
-        System.out.println("Hallo");
+        //System.out.println(shopService);
+        //System.out.println("Hallo");
+        List<Product> myOrder = new ArrayList<>();
+
+        //myOrder.add(new Product("2", "Banane"));
+
+        Order ordertwo = new Order("453",myOrder,OrderStatus.PROCESSING);
+        shopService.addOrder(List.of("2","1"));
+        //System.out.println(shopService.getOrderRepo().getOrders());
+        System.out.println(shopService.getOrdersByOrderStatus(OrderStatus.PROCESSING));
 
 
     }

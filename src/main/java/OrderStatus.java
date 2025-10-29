cef458ffb27030ec47fa22f05732ff64698304a2
Ismail Erdogan <erdogan.isma@gmail.com>
2025-10-29 07:05:11 +0100

    OrderStatus implementiert

--- src/main/java/OrderStatus.java
+++ src/main/java/OrderStatus.java
@@ -1,5 +1,11 @@
 public enum OrderStatus {
-    PROCESSING,
-    IN_DELIVERY,
-    COMPLETED
+    PROCESSING("Processing"),
+    IN_DELIVERY("In_Delivery"),
+    COMPLETED("Completed");
+
+    private final String value;
+
+    OrderStatus(String value){
+        this.value = value;
+    }
 }
